209bc6ed3736b08f9590b10bfbdb0e00a477c0bc
VitaoL <62264826+VitaoL@users.noreply.github.com>
2026-01-02 17:51:13 -0300

    Fix services mock exports

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -5,10 +5,7 @@ export {
   fetchAboutData,
   fetchAboutPhotoHighlight,
 } from "./about";
-export {
-  fetchServicesData,
-  fetchContractorCompaniesData,
-} from "./services";
+export { fetchContractorCompaniesData } from "./services";
 export { fetchFinalClientsData } from "./clients";
 export {
   fetchDeliverablesData,
